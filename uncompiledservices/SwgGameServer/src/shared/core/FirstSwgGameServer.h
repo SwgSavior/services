50cbf3cab2aed67ba555faf7710e364d7cfe69a8
SWG Savior <zonomar@gmail.com>
2015-05-23 21:52:55 -0400

    Fixed pathway error

--- uncompiledservices/SwgGameServer/src/shared/core/FirstSwgGameServer.h
+++ uncompiledservices/SwgGameServer/src/shared/core/FirstSwgGameServer.h
@@ -12,7 +12,7 @@
 
 #pragma warning ( disable : 4503 ) // name truncated (STL)
 #include "sharedFoundation/FirstSharedFoundation.h"
-#include "sharedDebug/FirstSharedDebug.h"
+#include "sharedFoundation/sharedMemoryManager/sharedDebug/FirstSharedDebug.h"
 #include "sharedMemoryManager/FirstSharedMemoryManager.h"
 
 #include "StringId.h"
